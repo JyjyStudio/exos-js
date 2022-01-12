3cf432d4d2aa42b4e21f35aee0278bc41db6bf45
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-01-12 15:29:07 +0100

    fonction priseRDV

--- rdv/function.js
+++ rdv/function.js
@@ -5,29 +5,39 @@ let messageRdvMax = `Les RDV doivent etre pris avant ${heureFinRdvMax}h${minuteF
 let finRdv;
 
 let priseRdv = (heure, minute) => {
-    if (minute < 60 && minute >= 0 && heure <= 16 && heure >= 8) {
+    var checkHeureMinute = () => {
         if (minute + dureeRdv >= 60 && minute + dureeRdv < 70) {
             finRdv = `${heure + 1}h0${minute + dureeRdv - 60}.`;
             console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`);
-        }else if (minute > 10) {
+        } else if (minute + dureeRdv >= 60 && minute + dureeRdv < 70 && minute > 10) {
             finRdv = `${heure}h${minute + dureeRdv}.`;
             console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`);
+        }
+        else if (minute >= 0 && minute < 60 && heure >= 8 && heure < 16) {
+            checkHeureMinute();
         }
         else {
-            finRdv = `${heure}h${minute + dureeRdv}.`;
-            console.log(`Votre rdv a bien été pris à ${heure}h0${minute}, il se terminera à ${finRdv}`);
+            console.log('Veuillez verifier votre demande de rdv.');
         }
     }
-    else if (heure == 16) {
-        if (minute + dureeRdv <= minuteFinRdvMax) {
-            console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`);
-        } else console.log(messageRdvMax)
+     if (heure == 16) {
+        checkHeureMinute();
+        // if (minute + dureeRdv <= minuteFinRdvMax) {
+        //     console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`);
+        // } else console.log(messageRdvMax)
     }
     else if (heure < 8) console.log('Les RDV commencent à 8h00.');
     else if (heure > 16) console.log(messageRdvMax);
-    else console.log('Veuillez verifier votre demande de rdv.');
 }
 
 //prise de rdv entre 8h et 16h20. Duree du rdv 10 minutes. Affiche l'heure de debut et de fin de rdv.
-
+/*
+contraintes : 
+    -heure de rdv entre 8h00 et 16h30
+    -apres 60 minutes on remet les minutes à 0 et on ajoute 1h exemple : 15h55 + 15 minutes = 16h10 
+    -le dernier rdv doit etre (dureeRdv - heure de fin de journée) ici : à 16h30 - 15 minutes
+    -on doit afficher l'heure de debut et fin du rdv
+    -afficher une erreur si en dehors des heure de travail exemple 'Les RDV commencent à 8h' et 'Les RDV doivent etre pris avant 16h20'
+    -formatter l'heure pour afficher par exemple 15h09 et non 15h9
+*/
 priseRdv(16, 9)
