7f0a552df920a9f762bdff98694ce4a573869430
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-01-13 21:54:08 +0100

    énnoncé modifié

--- rdv/function.js
+++ rdv/function.js
@@ -38,16 +38,16 @@ let priseRdv = (heure, minute) => {
     else if (heure > 16) console.log(messageRdvMax);
 }
 
-//prise de rdv entre 8h et 16h20. Duree du rdv 10 minutes. Affiche l'heure de debut et de fin de rdv.
-/*
+/*prise de rdv entre 8h et 16h20. Duree du rdv de 10 minutes (variable). La fonction priseRdv() affiche l'heure de debut et de fin de rdv.
 contraintes : 
-    -heure de rdv entre 8h00 et 16h30
-    -apres 60 minutes on remet les minutes à 0 et on ajoute 1h exemple : 15h55 + 15 minutes = 16h10 
-    -le dernier rdv doit etre (dureeRdv - heure de fin de journée) ici : à 16h30 - 15 minutes
-    -on doit afficher l'heure de debut et fin du rdv
-    -afficher une erreur si en dehors des heure de travail exemple 'Les RDV commencent à 8h' et 'Les RDV doivent etre pris avant 16h20'
-    -formatter l'heure pour afficher par exemple 15h09 et non 15h9
+    -on doit afficher l'heure de debut et fin du rdv (console)
+    -les heure de rdv doivent etre entre 8h00 et 16h30
+    -apres 60 minutes il faut remettre les minutes à 0 et ajouter 1h, exemple : 15h55 + 15 minutes = 16h10 et non pas 15h70 
+    -le dernier rdv doit etre à 16h30 - 15 minutes, l'heure de fin de rdv et la durée des rdv peuvent varier
+    -afficher une erreur si la demande est en dehors des heure de travail, exemple 'Les RDV commencent à 8h' et 'Les RDV doivent etre pris avant 16h20'
     -faire des demandes entre 0h et 24h et des minutes entre 0 et 60 (pas de demande à 38h78)
-    -Pour tester la fonction: entrer en parametre 16h00, 16h10, 16h20, 16h30, 16h50, 15h40, 15h50, 15h55, 7h00, 7h50, 8h, 8h10, 8h50, 17h, 17h90 et verifier les valeurs
+    -formatter l'heure pour afficher par exemple 15h09 et non 15h9
+    -Pour tester la fonction: entrer en parametre 7h00, 7h50, 8h, 8h10, 8h50, 15h40, 15h50, 15h55, 17h, 17h90, 16h00, 16h10, 16h20, 16h30, 16h50 et verifier les valeurs
 */
+
 priseRdv(27, 0)
