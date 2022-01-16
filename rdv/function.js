8164bcfae16df0b4132717063fcfa422333ab7d3
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-01-16 11:59:32 +0100

    resultat dans la page html

--- rdv/function.js
+++ rdv/function.js
@@ -1,43 +1,3 @@
-let heureFinRdvMax = 16;
-let minuteFinRdvMax = 30;
-let dureeRdv = 10;
-let messageRdvMax = `Les RDV doivent etre pris avant ${heureFinRdvMax}h${minuteFinRdvMax - dureeRdv}.`;
-let finRdv;
-
-let priseRdv = (heure, minute) => {
-    let checkMinute = () => {
-        if (minute + dureeRdv >= 60 && minute + dureeRdv < 70) {
-            return finRdv = `${heure + 1}h0${minute + dureeRdv - 60}.`;
-            // console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`);
-        } else if (minute + dureeRdv >= 60 && minute + dureeRdv < 70 || minute > 10) {
-            return finRdv = `${heure}h${minute + dureeRdv}.`;
-            // console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`);
-        }
-        else if (minute >= 0 && minute < 60) {
-            return finRdv = `${heure}h${minute + dureeRdv}.`;
-        }
-    }
-
-    if (minute >= 60 || heure > 23 || heure < 0) {
-        console.log('Veuillez verifier votre demande de rdv.');
-    }
-    else if (heure >= 8 && heure <= 16) {
-        if (heure == 16) {
-            if (minute + dureeRdv <= minuteFinRdvMax) {
-                checkMinute();
-                if (minute < 10) console.log(`Votre rdv a bien été pris à ${heure}h0${minute}, il se terminera à ${finRdv}`)
-                else console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`)
-            } else console.log(messageRdvMax)
-        } else {
-            checkMinute();
-            if (minute < 10) console.log(`Votre rdv a bien été pris à ${heure}h0${minute}, il se terminera à ${finRdv}`)
-            else console.log(`Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}`)
-        }
-    }
-    else if (heure < 8) console.log('Les RDV commencent à 8h00.');
-    else if (heure > 16) console.log(messageRdvMax);
-}
-
 /*prise de rdv entre 8h et 16h20. Duree du rdv de 10 minutes (variable). La fonction priseRdv() affiche l'heure de debut et de fin de rdv.
 contraintes : 
     -on doit afficher l'heure de debut et fin du rdv (console)
@@ -50,4 +10,63 @@ contraintes :
     -Pour tester la fonction: entrer en parametre 7h00, 7h50, 8h, 8h10, 8h50, 15h40, 15h50, 15h55, 17h, 17h90, 16h00, 16h10, 16h20, 16h30, 16h50 et verifier les valeurs
 */
 
-priseRdv(27, 0)
+
+
+let heureFinRdvMax = 16;
+let minuteFinRdvMax = 30;
+let dureeRdv = 10;
+let messageRdvMax = `Les RDV doivent etre pris avant ${heureFinRdvMax}h${minuteFinRdvMax - dureeRdv}.`;
+let finRdv;
+
+let priseRdv = (heure, minute) => { 
+    let checkMinute = () => {
+        if (minute + dureeRdv >= 60 && minute + dureeRdv < 70) {
+            return finRdv = `${heure + 1}h0${minute + dureeRdv - 60}.`;
+        } else if (minute + dureeRdv >= 60 && minute + dureeRdv < 70 || minute > 10) {
+            finRdv = `${heure}h${minute + dureeRdv}.`;
+            response.innerHTML = finRdv;
+        }
+        else if (minute >= 0 && minute < 60) {
+            return finRdv = `${heure}h${minute + dureeRdv}.`;
+        }
+    }
+
+    if (minute >= 60 || heure > 23 || heure < 0) {
+        response.innerHTML = `<p class="red">Veuillez verifier votre demande de rdv.</p>`;
+    }
+    else if (heure >= 8 && heure <= 16) {
+        if (heure == 16) {
+            if (minute + dureeRdv <= minuteFinRdvMax) {
+                checkMinute();
+                if (minute < 10) response.innerHTML = `<p class="green">Votre rdv a bien été pris à ${heure}h0${minute}, il se terminera à ${finRdv}</p>`;
+                else response.innerHTML = `<p class="green">Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}</p>`;
+            } else response.innerHTML = `<p class="red">${messageRdvMax}</p>`
+        } else {
+            checkMinute();
+            if (minute < 10) {
+                finRdv = `${heure}h${minute + dureeRdv}.`;
+                response.innerHTML = `<p class="green">Votre rdv a bien été pris à ${heure}h0${minute}, il se terminera à ${finRdv}</p>`;
+                }
+            else response.innerHTML = `<p class="green">Votre rdv a bien été pris à ${heure}h${minute}, il se terminera à ${finRdv}</p>`;
+        }
+    }
+    else if (heure < 8) response.innerHTML = '<p class="red">Les RDV commencent à 8h00.</p>';
+    else if (heure > 16) response.innerHTML = `<p class="red">${messageRdvMax}</p>`;
+}
+
+let form = document.getElementById("form");
+let inputHeure = document.getElementById('heure');
+let inputMinute = document.getElementById('minute');
+response = document.getElementById("response");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let heure = Number(inputHeure.value);
+    let minute = Number(inputMinute.value);
+    priseRdv(heure, minute)
+    }
+);
+
+
+
+priseRdv(11,20)
