ea1692779a01a6f00e541fc40f65d703042bee6d
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-01-18 00:00:18 +0100

    modif du 17/01

---
+++ maxNum/function.js
@@ -0,0 +1,69 @@
+// //Le plus grand chiffre
+function maxNum(a, ...b) {
+    console.log(`Le plus grand chiffre est ${Math.max(a, ...b)}.`);
+}
+maxNum(3, 56, 20, 2, 21, 222);
+
+//Le plus petit chiffre
+function minNum(a, ...b) {
+    console.log(`Le plus petit chiffre est ${Math.min(a, ...b)}.`);
+}
+minNum(3, 56, 20, 2, 21, 222);
+
+// //Addition avec le rest parameter
+function addition(...a) {
+    return a.reduce((accumulator, number) => {
+        return accumulator += number;
+    });
+}
+let exemple1 = addition(1, 2); // 3
+let exemple2 = addition(1, 2, 3, 4, 5); // 15
+console.log(exemple1);
+console.log(exemple2);
+
+// //fonction reduce v1
+const getMapFromArray = data =>
+    data.reduce((accu, item) => {
+        // add object key to our object i.e. charmander: { type: 'water' }
+        accu[item.name] = { type: item.type };
+        return accu;
+    }, {});
+
+let pokemon = [
+    { name: "Salameche", type: "Feu" },
+    { name: "Carapuce", type: "Eau" },
+    { name: "Bulbizarre", type: "Plante" }
+];
+
+let pokemonModified = getMapFromArray(pokemon);
+
+console.log(pokemon);
+console.log(pokemonModified);
+
+// //fonction reduce V2
+const getMapFromArray2 = pokemon.reduce((accu, item) => {
+    // add object key to our object i.e. charmander: { type: 'water' }
+    accu[item.name] = { type: item.type };
+    return accu;
+}, {});
+
+console.log(getMapFromArray2);
+
+//reunir les personnes par age 
+const personnes = [
+    { prenom: "Jyhad", age: 31 },
+    { prenom: "Toto", age: 26 },
+    { prenom: "Tata", age: 26 },
+    { prenom: "foo", age: 35 },
+    { prenom: "bar", age: 42 }
+];
+
+const result = personnes.reduce((groupePersonnes, personnes) => {
+    const age = personnes.age;
+    if (groupePersonnes[age] == null) groupePersonnes[age] = [];
+    groupePersonnes[age].push(personnes);
+    return groupePersonnes;
+}, {});
+
+console.log(result);
+
--- random_gallery/js/singleGallery.js
+++ random_gallery/js/singleGallery.js
@@ -1,20 +1,46 @@
+let galleriesRaw = '{"gal1": {"title": "Bretagne 2019", "images": ["image-1.jpg","image-2.jpg","image-3.jpg","image-4.jpg","image-5.jpg","image-6.jpg"]},"gal2":{ "title": "Sacramento 2017", "images": ["image-7.jpg","image-8.jpg","image-9.jpg","image-10.jpg","image-11.jpg","image-12.jpg","image-13.jpg","image-14.jpg"]}}';
 let toutesLesImages = document.querySelectorAll('#singleGallery img');
+console.log(window.location.search)
 
 for (let i = 0; i < toutesLesImages.length; i++) {
-    let image = toutesLesImages[i];
+    var image = toutesLesImages[i];
     image.addEventListener('click', showPict);
 }
 
+//Pour afficher les images en grand lors du click 
 function showPict(e) {
     let image = e.target;
-    let bigImageContainer = document.getElementById('galleryContainer');
-    let bigImage = bigImageContainer.querySelector('img');
+    let bigImage = document.querySelector('#galleryContainer img');
     bigImage.src = image.src;
+    let bigImageContainer = document.getElementById('galleryContainer');
     bigImageContainer.classList.toggle('visible');
     bigImageContainer.addEventListener('click', closePict);
 }
 
+//pour reduire la photo onclick
 function closePict() {  
     let bigImageContainer = document.getElementById('galleryContainer');
-    bigImageContainer.classList.toggle('visible')
+    bigImageContainer.classList.toggle('visible');
 }
+
+//affiche le bon titre
+let h1 = document.querySelector('#singleGallery h1');
+let galleries = JSON.parse(galleriesRaw);
+console.log(galleries)
+if (document.location.search.includes('gal2')) {
+    h1.textContent = galleries.gal2.title;
+}
+
+//affiche la bonne gallerie
+let gal2 = galleries.gal2.images;
+console.log(gal2)
+for (let i = 0; i < gal2.length -2; i++) {
+    let image = gal2[i];
+    if (document.location.search.includes('gal2')) {
+        console.log(image)
+        toutesLesImages[i].src = `images/${image}`
+    } 
+}
+
+let url3 = window.location.search.get('id');
+console.log(url3)
