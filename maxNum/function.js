8ad9ade446c49ff0d74e53da39ced704452a8ab5
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-01-19 22:53:23 +0100

    tests

--- maxNum/function.js
+++ maxNum/function.js
@@ -1,17 +1,13 @@
 //Le plus grand chiffre
-function maxNum(a, ...b) {
-    console.log(`Le plus grand chiffre est ${Math.max(a, ...b)}.`);
-}
+const maxNum = (a, ...b) => console.log(`Le plus grand chiffre est ${Math.max(a, ...b)}.`);
 maxNum(3, 56, 20, 2, 21, 222);
 
 //Le plus petit chiffre
-function minNum(a, ...b) {
-    console.log(`Le plus petit chiffre est ${Math.min(a, ...b)}.`);
-}
+const minNum = (a, ...b) => console.log(`Le plus petit chiffre est ${Math.min(a, ...b)}.`);
 minNum(3, 56, 20, 2, 21, 222);
 
 //Addition avec le rest parameter
-function addition(...a) {
+const addition = (...a) => {
     return a.reduce((accumulator, number) => {
         return accumulator += number;
     });
@@ -22,8 +18,7 @@ console.log(exemple1);
 console.log(exemple2);
 
 //fonction reduce v1
-const getMapFromArray = data =>
-    data.reduce((accu, item) => {
+const getMapFromArray = data => data.reduce((accu, item) => {
         // add object key to our object i.e. charmander: { type: 'water' }
         accu[item.name] = { type: item.type };
         return accu;
@@ -36,7 +31,6 @@ let pokemon = [
 ];
 
 let pokemonModified = getMapFromArray(pokemon);
-
 console.log(pokemon);
 console.log(pokemonModified);
 
@@ -67,3 +61,19 @@ const result = personnes.reduce((groupePersonnes, personnes) => {
 
 console.log(result);
 
+//chainage optionnel 
+const country = {
+    name : 'Finland',
+    population : 500000,
+    shores : true,
+    forest : true, 
+    welcome() {console.log('Tervetuola suomeen')}, //Bienvenue en Finlande
+    superficy : 338440, 
+    borders : ['Sweden', 'Russia', 'Norway']
+};
+console.log(country);
+country.welcome?.();//si la fonction welcome existe => tu l'excecute
+console.log(country?.population);// si country existe affiche moi population
+country.population?console.log(country.population) : ""; //equivalent a la ligne du dessus
+console.log(country.borders?.[0]); // si la prop borders existe alors affiche moi son premier element
+country.borders?.forEach(border => console.log(border)); //si border alors affiche moi ttes les borders 
