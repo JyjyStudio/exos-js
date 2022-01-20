deeda15a0fcec9a3f7c91d66cbcc37ae7d7b6e63
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-01-20 19:57:31 +0100

    file renamed

--- rest parameter/rest parameter.js
+++
@@ -1,9 +0,0 @@
-function addition(...nombres) {
-    let resultat = 0;
-    nombres.forEach(nombre => {
-        resultat += nombre;
-    })
-    console.log(resultat);
-}
-
-addition(1,2,3,4,5,6584,6,55,8,5,2,1,4,5,227854,26545,8,5,452,1452)
