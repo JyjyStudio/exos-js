d2be6875181a1c3ad4c0d27e71169b01bf83821c
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-01-27 23:58:57 +0100

    refactorisation du code

--- fetch/code_postal/app.ts
+++ fetch/code_postal/app.ts
@@ -10,23 +10,20 @@ input.addEventListener('keyup', async function() {
     ul.innerHTML = "";
     this.reportValidity();
     if(input.value.length == 5) {
-        let li = document.createElement('li') as HTMLElement;
         let url = `https://geo.api.gouv.fr/communes?codePostal=${input.value}&fields=nom,code,codesPostaux,codeDepartement,codeREgion,population&format=json&geometry=centre` as string;
         try {
             let data = await fetch(url);
             let response = await data.json();
             if(response.length) {
                 response.forEach((ville : Ville) => {
-                    li.innerHTML += `<li><b>${ville.nom}</b> - ${ville.population} habitants.</li>`
+                    ul.innerHTML += `<li><b>${ville.nom}</b> - ${ville.population} habitants.</li>`
                 });
             }else {
-                li.innerHTML += `<li><b>Il n'y a pas de ville avec ce code postal.</li>`
+                ul.innerHTML += `<li><b>Il n'y a pas de ville avec ce code postal.</li>`
             }
-        }catch (e) {
-            console.error("Voici l'erreur: ", e)
-            li.innerHTML += `<li><b>Il y a eu une erreur.</li>`
+        }catch {
+            ul.innerHTML += `<li><b>Il y a eu une erreur.</li>`
         } finally {
-            ul.appendChild(li);
             result.appendChild(ul);
         }
     }
