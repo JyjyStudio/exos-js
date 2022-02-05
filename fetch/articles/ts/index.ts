4543cb9e65425be87fcc1184c9f88ac4f788196a
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-02-05 23:55:03 +0100

    tentative de templating

--- fetch/articles/ts/index.ts
+++ fetch/articles/ts/index.ts
@@ -1,13 +1,14 @@
-let templateDiv = document.getElementById('template') as HTMLElement;
-let apiUrl = `https://jsonplaceholder.typicode.com/posts`;
+const container = document.getElementById('container') as HTMLElement;
+const templateDiv = document.getElementById('template') as HTMLElement;
+const apiUrl = `https://jsonplaceholder.typicode.com/posts`;
 
-interface Artcicle {
+interface Article {
     title : string,
     body: string,
     id : number
 }
 
-function displayArticles (article:Artcicle) {
+function displayArticlesV1 (article:Article) {
     templateDiv.innerHTML += `
         <div id="article" class="article">
             <h2 id="articleTitle">${article.title}</h2>
@@ -15,14 +16,39 @@ function displayArticles (article:Artcicle) {
             <a href="article.html?id=${article.id}" id="articleLink">Voir l'article</a>
         </div> `
 }
-
-const  getArticles = async() => {
-    let response = await fetch(apiUrl);
-    let data = await response.json();
-    console.log(data[0]);
+function displayArticlesV2 (article:Article) {
+    const template = document.getElementById('templateArticle') as HTMLTemplateElement;
+    const clone = document.importNode(template.content, true) as any;
     
-    for (let i = 0; i < 6; i++) {
-        displayArticles(data[i])
+    clone.getElementById('articleTitle').textContent = article.title;
+    clone.getElementById('content').textContent = article.body;
+    
+    container.appendChild(clone);
+}
+
+// const  getArticles = async() => {
+//     const response = await fetch(apiUrl);
+//     const data = await response.json()
+//     .then(function (articles) {return articles})
+// }
+// getArticles();
+async function getArticles() {
+    try {
+        const httpBodyResponse = await fetch(apiUrl);
+        const articles = await httpBodyResponse.json();
+        return articles;
+    } catch (error) {
+        alert(error);
     }
 }
-getArticles();
+
+async function main() {
+    const articles = await getArticles();
+    console.log(articles[0].title);
+    
+    for (let i = 0; i < 6; i++) {
+        //     displayArticlesV1(data[i])
+            displayArticlesV2(articles[i])
+        }
+  }
+  main()
