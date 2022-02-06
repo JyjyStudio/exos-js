e1f4f6ec3cba23be9d85185f66bc0b3c92bf7f49
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-02-06 16:12:21 +0100

    code amélioré et utilisation de template

--- fetch/articles/ts/index.ts
+++ fetch/articles/ts/index.ts
@@ -2,6 +2,16 @@ const container = document.getElementById('container') as HTMLElement;
 const templateDiv = document.getElementById('template') as HTMLElement;
 const apiUrl = `https://jsonplaceholder.typicode.com/posts`;
 
+const  getArticles = async() => {
+    try {
+        const response = await fetch(apiUrl);
+        const articles = await response.json();
+        return articles;
+    } catch (error) {
+        alert(error);
+    }
+}
+
 interface Article {
     title : string,
     body: string,
@@ -14,41 +24,25 @@ function displayArticlesV1 (article:Article) {
             <h2 id="articleTitle">${article.title}</h2>
             <p id="content">${article.body}</p>
             <a href="article.html?id=${article.id}" id="articleLink">Voir l'article</a>
-        </div> `
+        </div> `;
 }
+
 function displayArticlesV2 (article:Article) {
     const template = document.getElementById('templateArticle') as HTMLTemplateElement;
-    const clone = document.importNode(template.content, true) as any;
-    
-    clone.getElementById('articleTitle').textContent = article.title;
-    clone.getElementById('content').textContent = article.body;
-    
+    const clone = document.importNode(template.content, true) as DocumentFragment;
+    const link = clone.getElementById('articleLink') as HTMLLinkElement;
+
+    clone.getElementById('articleTitle')!.textContent = article.title;
+    clone.getElementById('content')!.textContent = article.body;
+    link.href = `article.html?id=${article.id}`;
+
     container.appendChild(clone);
 }
 
-// const  getArticles = async() => {
-//     const response = await fetch(apiUrl);
-//     const data = await response.json()
-//     .then(function (articles) {return articles})
-// }
-// getArticles();
-async function getArticles() {
-    try {
-        const httpBodyResponse = await fetch(apiUrl);
-        const articles = await httpBodyResponse.json();
-        return articles;
-    } catch (error) {
-        alert(error);
+async function renderArticles() {
+    const articles = await getArticles();
+    for (let i = 0; i < 5; i++) {
+        displayArticlesV2(articles[i]);
     }
 }
-
-async function main() {
-    const articles = await getArticles();
-    console.log(articles[0].title);
-    
-    for (let i = 0; i < 6; i++) {
-        //     displayArticlesV1(data[i])
-            displayArticlesV2(articles[i])
-        }
-  }
-  main()
+renderArticles();
